7c8223d9895c6dc7c557129942908928fb69caab
Max van der Kolk <max.vanderkolk@vortech.nl>
2024-04-30 14:18:22 +0200

    test: Introduce end-to-end testing for vlaanderen
    This implements a basic end to end test for ikob, taking commit 110b99d
    ("Add end-to-end test", 2022-11-29) as an initial reference
    implementation.
    
    For a given project configuration, the end-to-end test will run through
    all steps of the ikob process using the isolated iteration utility of
    the previous commit. The test assert successful completion of each step
    and will perform a complete directory comparison on the resulted output
    files with respect to the stored reference files.
    
    The Excel files are compared using their Pandas representations through
    `pandas.testing.assert_frame_equal` considering the default settings for
    absolute and relative tolerances on floating point comparisons.

---
+++ tests/test_end_to_end.py
@@ -0,0 +1,89 @@
+from ikob.ikobrunner import stappen, run_scripts
+import pathlib
+import pytest
+import filecmp
+import shutil
+import pandas as pd
+
+
+def is_equal_excel_file(result, reference) -> bool:
+    """Compare two xlsx files within numerical tolerances.
+
+    The files are considered equal if their pandas.DataFrame representations
+    are considered equal up to given relative and absolute tolerance differences.
+    """
+    result_frame = pd.read_excel(result)
+    reference_frame = pd.read_excel(reference)
+    # These match the defaults of assert_frame_equal.
+    rtol = 1e-5
+    atol = 1e-8
+    try:
+        pd.testing.assert_frame_equal(result_frame, reference_frame, rtol=rtol, atol=atol)
+        return True
+    except AssertionError as err:
+        print(f"Excel file {result} differs from reference:\n{err}")
+        return False
+
+
+def is_equal_file(dcmp: filecmp.dircmp, file: pathlib.Path) -> bool:
+    """Compare files in detail with filetype specific comparisons."""
+
+    result = pathlib.Path(dcmp.left).joinpath(file)
+    reference = pathlib.Path(dcmp.right).joinpath(file)
+
+    if file.suffix == ".xlsx":
+        return is_equal_excel_file(result, reference)
+
+    return False
+
+
+def same_directory(dcmp: filecmp.dircmp) -> bool:
+    """Recursively compare directories for differing files."""
+
+    # File is only present in one of the directory trees.
+    if dcmp.left_only or dcmp.right_only:
+        return False
+
+    for filepath in dcmp.diff_files:
+        if not is_equal_file(dcmp, pathlib.Path(filepath)):
+            return False
+
+    # Recursively compare directories.
+    for sub_dcmp in dcmp.subdirs.values():
+        if not same_directory(sub_dcmp):
+            return False
+
+    return True
+
+
+def compare_directories(result, reference) -> bool:
+    assert result.is_dir(), f"Result directory {result} should exist."
+    assert reference.is_dir(), f"Reference directory {result} should exist."
+    return same_directory(filecmp.dircmp(result, reference))
+
+
+def remove_directory(dir: pathlib.Path):
+    if dir.exists() and dir.is_dir():
+        shutil.rmtree(dir)
+
+
+@pytest.mark.parametrize("case", ["vlaanderen"])
+def test_end_to_end(case):
+    test_dir = pathlib.Path("tests")
+    project_dir = test_dir.joinpath(case).resolve()
+    project = project_dir.joinpath(f"{case}.json")
+    result_dir = project_dir.joinpath(case).joinpath("Resultaten")
+
+    # Delete old results if still present
+    remove_directory(result_dir)
+
+    # End-to-end test should not skip any steps: all scripts should pass.
+    skip_tests = [False for _ in stappen]
+    for step, return_code in run_scripts(project, skip_tests):
+        assert return_code == 0, f"Step {step} should pass."
+
+    reference_dir = project_dir.joinpath("reference").joinpath("Resultaten")
+    assert compare_directories(result_dir, reference_dir)
+
+    # Clean up files if test succeeds
+    remove_directory(result_dir)
