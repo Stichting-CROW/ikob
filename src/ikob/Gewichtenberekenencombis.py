162a3c1ae8c04f27d711e18872ca696d76e78bbb
Max van der Kolk <max.vanderkolk@vortech.nl>
2024-05-22 14:23:03 +0200

    refactor: Explicitly skip loops

--- src/ikob/Gewichtenberekenencombis.py
+++ src/ikob/Gewichtenberekenencombis.py
@@ -4,11 +4,11 @@ import ikob.Routines as Routines
 logger = logging.getLogger(__name__)
 
 
-def kanvoorkeur(soortauto, soortOV, voorkeur) :
+def kanvoorkeur(soortauto, soortOV, voorkeur):
     if soortauto == 'GeenAuto' or soortauto == 'GeenRijbewijs':
         if voorkeur == 'Auto':
             return False
-        else :
+        else:
             if soortOV == 'GratisOV':
                 if voorkeur != 'OV':
                     return False
@@ -20,19 +20,16 @@ def kanvoorkeur(soortauto, soortOV, voorkeur) :
         if soortOV == 'GratisOV':
             if voorkeur != 'Neutraal':
                 return False
-            else :
+            else:
                 return True
         else:
-            if voorkeur != 'Auto' :
+            if voorkeur != 'Auto':
                 return False
-            else :
+            else:
                 return True
-    elif soortOV == 'GratisOV' :
-                if voorkeur != 'OV':
-                    return False
-                else :
-                    return True
-    else :
+    elif soortOV == 'GratisOV':
+        return voorkeur == 'OV'
+    else:
         return True
 
 
@@ -44,7 +41,6 @@ def gewichten_berekenen_combis(config, datasource):
     motieven = project_config['motieven']
     regime = project_config['beprijzingsregime']
     dagsoort = skims_config['dagsoort']
-    #Scenario = config['project']['scenario']
 
     # Vaste waarden
     inkomen = ['hoog', 'middelhoog', 'middellaag', 'laag']
@@ -52,8 +48,7 @@ def gewichten_berekenen_combis(config, datasource):
     modaliteitenfiets = ['Fiets']
     soortauto = ['Auto', 'GeenAuto', 'GeenRijbewijs', 'GratisAuto']
     soortOV = ['OV', 'GratisOV']
-    soortbrandstof = ['fossiel','elektrisch']
-
+    soortbrandstof = ['fossiel', 'elektrisch']
 
     for mot in motieven:
         for ds in dagsoort:
@@ -61,61 +56,62 @@ def gewichten_berekenen_combis(config, datasource):
                 for vk in voorkeuren:
                     for modft in modaliteitenfiets:
                         for srtOV in soortOV:
-                            if kanvoorkeur ('Auto', srtOV, vk):
-                                if vk == 'Fiets':
-                                    vkklad = 'Fiets'
-                                else:
-                                    vkklad = ''
-                                Fietsmatrix = datasource.read_csv('Gewichten', f'{modft}_vk',ds,vk=vkklad, regime=regime, mot=mot)
-                                OVmatrix = datasource.read_csv('Gewichten', f'{srtOV}_vk', ds, vk=vk, ink=ink, regime=regime, mot=mot)
-                                max = Routines.maxmatrix(Fietsmatrix, OVmatrix)
-                                datasource.write_csv(max, 'Gewichten', f"{srtOV}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot)
+                            if not kanvoorkeur('Auto', srtOV, vk):
+                                continue
+
+                            vkfiets = 'Fiets' if vk == 'Fiets' else ''
+                            Fietsmatrix = datasource.read_csv('Gewichten', f'{modft}_vk', ds, vk=vkfiets, regime=regime, mot=mot)
+                            OVmatrix = datasource.read_csv('Gewichten', f'{srtOV}_vk', ds, vk=vk, ink=ink, regime=regime, mot=mot)
+                            max = Routines.maxmatrix(Fietsmatrix, OVmatrix)
+                            datasource.write_csv(max, 'Gewichten', f"{srtOV}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot)
+
                         for srtauto in soortauto:
-                            if kanvoorkeur (srtauto, 'OV', vk):
-                                if vk == 'Fiets':
-                                    vkklad = 'Fiets'
-                                else:
-                                    vkklad = ''
-                                Fietsmatrix = datasource.read_csv('Gewichten', f'{modft}_vk',ds, vk=vkklad, regime=regime, mot=mot)
-                                if srtauto == 'Auto':
-                                    for srtbr in soortbrandstof:
-                                        Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk',ds,vk=vk,ink=ink, srtbr=srtbr, mot=mot, regime=regime)
-                                        max = Routines.maxmatrix(Fietsmatrix, Automatrix)
-                                        datasource.write_csv(max, 'Gewichten', f"{srtauto}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot, srtbr=srtbr)
-                                else:
-                                    Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk',ds,vk=vk,ink=ink, mot=mot, regime=regime)
+                            if not kanvoorkeur(srtauto, 'OV', vk):
+                                continue
+
+                            vkfiets = 'Fiets' if vk == 'Fiets' else ''
+                            Fietsmatrix = datasource.read_csv('Gewichten', f'{modft}_vk', ds, vk=vkfiets, regime=regime, mot=mot)
+                            if srtauto == 'Auto':
+                                for srtbr in soortbrandstof:
+                                    Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk', ds, vk=vk, ink=ink, srtbr=srtbr, mot=mot, regime=regime)
                                     max = Routines.maxmatrix(Fietsmatrix, Automatrix)
-                                    datasource.write_csv(max, 'Gewichten', f"{srtauto}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot)
+                                    datasource.write_csv(max, 'Gewichten', f"{srtauto}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot, srtbr=srtbr)
+                            else:
+                                Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk', ds, vk=vk, ink=ink, mot=mot, regime=regime)
+                                max = Routines.maxmatrix(Fietsmatrix, Automatrix)
+                                datasource.write_csv(max, 'Gewichten', f"{srtauto}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot)
 
                     for srtOV in soortOV:
                         for srtauto in soortauto:
-                            if kanvoorkeur (srtauto, srtOV, vk):
-                                OVmatrix = datasource.read_csv('Gewichten', f'{srtOV}_vk',ds,vk=vk,ink=ink,regime=regime, mot=mot)
-                                if srtauto == 'Auto':
-                                    for srtbr in soortbrandstof:
-                                        Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk',ds,vk=vk,ink=ink,srtbr=srtbr, regime=regime, mot=mot)
-                                        max = Routines.maxmatrix(OVmatrix, Automatrix)
-                                        datasource.write_csv(max, 'Gewichten', f"{srtauto}_{srtOV}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot, srtbr=srtbr)
-                                else:
-                                    Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk',ds,vk=vk,ink=ink, regime=regime, mot=mot)
+                            if not kanvoorkeur(srtauto, srtOV, vk):
+                                continue
+
+                            OVmatrix = datasource.read_csv('Gewichten', f'{srtOV}_vk', ds, vk=vk, ink=ink, regime=regime, mot=mot)
+                            if srtauto == 'Auto':
+                                for srtbr in soortbrandstof:
+                                    Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk', ds, vk=vk, ink=ink, srtbr=srtbr, regime=regime, mot=mot)
                                     max = Routines.maxmatrix(OVmatrix, Automatrix)
-                                    datasource.write_csv(max, 'Gewichten', f"{srtauto}_{srtOV}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot)
+                                    datasource.write_csv(max, 'Gewichten', f"{srtauto}_{srtOV}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot, srtbr=srtbr)
+                            else:
+                                Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk', ds, vk=vk, ink=ink, regime=regime, mot=mot)
+                                max = Routines.maxmatrix(OVmatrix, Automatrix)
+                                datasource.write_csv(max, 'Gewichten', f"{srtauto}_{srtOV}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot)
+
                     for modft in modaliteitenfiets:
                         for srtOV in soortOV:
                             for srtauto in soortauto:
-                                if kanvoorkeur (srtauto, srtOV, vk):
-                                    if vk == 'Fiets':
-                                        vkklad = 'Fiets'
-                                    else:
-                                        vkklad = ''
-                                    Fietsmatrix = datasource.read_csv('Gewichten', f'{modft}_vk',ds,vk=vkklad, regime=regime, mot=mot)
-                                    OVmatrix= datasource.read_csv('Gewichten', f'{srtOV}_vk',ds,vk=vk,ink=ink, regime=regime, mot=mot)
-                                    if srtauto == 'Auto':
-                                        for srtbr in soortbrandstof:
-                                            Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk',ds,vk=vk,ink=ink,srtbr=srtbr, regime=regime, mot=mot)
-                                            max = Routines.maxmatrix(Automatrix, Fietsmatrix, OVmatrix)
-                                            datasource.write_csv(max,'Gewichten', f"{srtauto}_{srtOV}_{modft}_vk",ds,subtopic='Combinaties', vk=vk,ink=ink, regime=regime, mot=mot, srtbr=srtbr)
-                                    else:
-                                        Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk',ds,vk=vk,ink=ink, regime=regime, mot=mot)
+                                if not kanvoorkeur(srtauto, srtOV, vk):
+                                    continue
+
+                                vkfiets = 'Fiets' if vk == 'Fiets' else ''
+                                Fietsmatrix = datasource.read_csv('Gewichten', f'{modft}_vk', ds, vk=vkfiets, regime=regime, mot=mot)
+                                OVmatrix = datasource.read_csv('Gewichten', f'{srtOV}_vk', ds, vk=vk, ink=ink, regime=regime, mot=mot)
+                                if srtauto == 'Auto':
+                                    for srtbr in soortbrandstof:
+                                        Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk', ds, vk=vk, ink=ink, srtbr=srtbr, regime=regime, mot=mot)
                                         max = Routines.maxmatrix(Automatrix, Fietsmatrix, OVmatrix)
-                                        datasource.write_csv(max,'Gewichten', f"{srtauto}_{srtOV}_{modft}_vk",ds,subtopic='Combinaties', vk=vk,ink=ink, regime=regime, mot=mot)
+                                        datasource.write_csv(max, 'Gewichten', f"{srtauto}_{srtOV}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot, srtbr=srtbr)
+                                else:
+                                    Automatrix = datasource.read_csv('Gewichten', f'{srtauto}_vk', ds, vk=vk, ink=ink, regime=regime, mot=mot)
+                                    max = Routines.maxmatrix(Automatrix, Fietsmatrix, OVmatrix)
+                                    datasource.write_csv(max, 'Gewichten', f"{srtauto}_{srtOV}_{modft}_vk", ds, subtopic='Combinaties', vk=vk, ink=ink, regime=regime, mot=mot)
