766716b6fe1ef838bc87bf360d3d1ade8f6aa845
Max van der Kolk <max.vanderkolk@vortech.nl>
2024-06-18 13:48:34 +0200

    feat: Load Parkeerzoektijd or generate on the fly
    Although the parkeerzoektijden can be directly generated from the
    stedelijkheidsgraad, the parkeerzoektijden were explicitly stored on
    disk.
    
    This patch makes setting the parkeerzoektijden optional in the
    configuration by:
    
    1. Falling back to the expected parkeerzoektijden file when the
       specified file does not exist; or
    2. By generating the parkeerzoektijden when the parkeerzoektijden file
       is either not set or not encountered on disk.
    
    When both the parkeerzoektijden and stedelijkheidsgraad are missing an
    assertion is raised, as we cannot recover from both files missing.
    
    Corresponding tests are included to verify the expected
    parkeerzoektijden are return for a couple different configuration
    situations.

--- tests/test_stedelijkheid_converter.py
+++ tests/test_stedelijkheid_converter.py
@@ -1,10 +1,15 @@
+import pathlib
+import numpy as np
+
+import pytest
+from ikob.datasource import DataSource
+from ikob.ikobconfig import getConfigFromArgs
+from ikob.Routines import csvintlezen
 from ikob.stedelijkheidsgraad_to_parkeerzoektijden import (
     stedelijkheidfile_to_parkeerzoektijdenfile,
 )
 
 from tests.test_end_to_end import is_equal_file
-
-import pathlib
 
 
 def test_stedelijkheid_converter(tmp_path):
@@ -14,3 +19,41 @@ def test_stedelijkheid_converter(tmp_path):
 
     stedelijkheidfile_to_parkeerzoektijdenfile(input, tmp_path)
     assert is_equal_file(result, reference)
+
+
+def test_generate_parkeerzoektijden_datasource():
+    case = "vlaanderen"
+    project_dir = pathlib.Path("tests") / case
+    project_file = project_dir.joinpath(f"{case}.json")
+    reference = csvintlezen(project_dir / "SEGS" / "Parkeerzoektijd.csv")
+
+    # Read test file on disk given in configurationf file.
+    config = getConfigFromArgs(project_file)
+    datasource = DataSource(config, config["__filename__"])
+    assert np.all(reference == datasource.read_parkeerzoektijden())
+
+    # Remove path from config and fall back to expected location.
+    del config["skims"]["parkeerzoektijden_bestand"]
+    datasource = DataSource(config, config["__filename__"])
+    assert np.all(reference == datasource.read_parkeerzoektijden())
+
+    # Set config to unkown path, trigger conversion on the fly.
+    config["skims"]["parkeerzoektijden_bestand"] = "unset"
+    datasource = DataSource(config, config["__filename__"])
+    assert np.all(reference == datasource.read_parkeerzoektijden())
+
+
+def test_assert_failed_parkeerzoektijden_conversion():
+    case = "vlaanderen"
+    project_dir = pathlib.Path("tests") / case
+    project_file = project_dir.joinpath(f"{case}.json")
+
+    # Overwrite SEGS directory to trigger failure in converting
+    # parkeerzoektijden as stedelijkheidsgraag is not present.
+    config = getConfigFromArgs(project_file)
+    config["skims"]["parkeerzoektijden_bestand"] = "unset"
+    config["project"]["paden"]["segs_directory"] = "unset"
+
+    datasource = DataSource(config, config["__filename__"])
+    with pytest.raises(AssertionError):
+        _ = datasource.read_parkeerzoektijden()
