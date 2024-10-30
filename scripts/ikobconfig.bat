52d7362ea47aafb649ba0bafc0a867f0b88b0af6
Max van der Kolk <max.vanderkolk@vortech.nl>
2024-10-30 11:10:52 +0100

    feat: Add .bat script for setup and GUIs
    Three bat scripts are provided in the `scripts/` directory:
    
    - `setup.bat`: Prepare a virtual environment for the installation of
      IKOB and its dependencies.
    - `ikobconfig.bat`: Activate the virtual environment and run the IKOB
      config GUI.
    - `ikobrunner.bat`: Activate the virtual environment and run the IKOB
      runner GUI.

---
+++ scripts/ikobconfig.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set venvdir=venv
+
+:: Move to the root directory of IKOB.
+cd %~dp0\..
+
+:: Ensure the virtual environment is activated.
+if exist %venvdir% (
+    call %venvdir%\Scripts\activate.bat
+) else (
+    goto :VIRTUALENV_NOT_PRESENT
+)
+
+:: Launch the config GUI.
+python src\ikob\ikobconfig.py
+goto :DONE
+
+:VIRTUALENV_NOT_PRESENT
+echo No virtual environment '%venvdir%' was found.
+echo Please configure IKOB first using "setup.bat".
+
+:: Keep the prompt open.
+:DONE
+echo Press any key to close.
+pause >nul
+exit
