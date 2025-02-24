3ace3ceb65af869fe6f38623c043bbe85a3e5da0
Max van der Kolk <max.vanderkolk@vortech.nl>
2025-02-24 11:54:01 +0100

    fix: Define a single set of `base_groups`
    The dependence on `'alle groepen'` in `car_possession_groups` has been
    removed.

--- src/ikob/deployment_opportunities.py
+++ src/ikob/deployment_opportunities.py
@@ -26,32 +26,23 @@ def deployment_opportunities(config,
     income_groups = project_config['welke_inkomensgroepen']
     electric_percentage = distribution_config['Percelektrisch']
 
-    if 'alle groepen' in car_possession_groups:
-        base_groups = [
-            'GratisAuto',
-            'GratisAuto_GratisOV',
-            'WelAuto_GratisOV',
-            'WelAuto_vkAuto',
-            'WelAuto_vkNeutraal',
-            'WelAuto_vkFiets',
-            'WelAuto_vkOV',
-            'GeenAuto_GratisOV',
-            'GeenAuto_vkNeutraal',
-            'GeenAuto_vkFiets',
-            'GeenAuto_vkOV',
-            'GeenRijbewijs_GratisOV',
-            'GeenRijbewijs_vkNeutraal',
-            'GeenRijbewijs_vkFiets',
-            'GeenRijbewijs_vkOV']
-    else:
-        base_groups = [
-            'GratisAuto',
-            'GratisAuto_GratisOV',
-            'WelAuto_GratisOV',
-            'WelAuto_vkAuto',
-            'WelAuto_vkNeutraal',
-            'WelAuto_vkFiets',
-            'WelAuto_vkOV']
+    base_groups = [
+        'GratisAuto',
+        'GratisAuto_GratisOV',
+        'WelAuto_GratisOV',
+        'WelAuto_vkAuto',
+        'WelAuto_vkNeutraal',
+        'WelAuto_vkFiets',
+        'WelAuto_vkOV',
+        'GeenAuto_GratisOV',
+        'GeenAuto_vkNeutraal',
+        'GeenAuto_vkFiets',
+        'GeenAuto_vkOV',
+        'GeenRijbewijs_GratisOV',
+        'GeenRijbewijs_vkNeutraal',
+        'GeenRijbewijs_vkFiets',
+        'GeenRijbewijs_vkOV'
+    ]
 
     groups = []
     for income_group in income_groups:
